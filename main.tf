7c59204c8c50cbe5b69b30656f7b8515b7b9aae0
smit <smitkotkar42@gmail.com>
2023-07-05 08:46:41 +0530

    lj

--- main.tf
+++ main.tf
@@ -13,13 +13,23 @@ resource "aws_iam_user" "user1" {
   name = "Smit"
 }
 
+resource "aws_iam_user" "user2" {
+  name = "Ansh"
+}
+
 resource "aws_iam_group" "Group" {
   name = "Kotkar"
 }
 
 resource "aws_iam_group_membership" "example_membership" {
-  users = aws_iam_user.user1.name
-  group = aws_iam_group.Group.name
+  users = [
+    aws_iam_user.user1.name
+    aws_iam_user.user2.name
+  ]
+
+  group = [
+    aws_iam_group.Group.name
+  ]
 }
 
 resource "aws_iam_policy" "policy" {
