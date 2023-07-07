0fd56b6dae8c92f170c4060ac6960428acc24606
smit <smitkotkar42@gmail.com>
2023-07-07 20:47:20 +0530

    asg

--- 1_instance.tf
+++ 1_instance.tf
@@ -1,3 +1,10 @@
+# <block_type> <resource_type> <resource_name> {
+#
+#   
+#
+#}
+
+
 provider "aws" {
   region = "us-east-2"
 }
@@ -6,5 +13,34 @@ resource "aws_instance" "my-instance" {
     ami = "ami-0d1c47ab964ae2b87"
     key_name = "ohio"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["sg-00c1edbd5605281ea"]
+    vpc_security_group_ids = [ aws_security_group.my-sg.id ]
+    depends_on = [ aws_security_group.my-sg ]
+    tags = {
+        Name = "my-demo-instance"
+    }
+}
+
+resource "aws_security_group" "my-sg" {
+  name        = "all-allow-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-01b5d8eb556921c46"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
