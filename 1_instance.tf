3273eeb5211b1dacca1d2d1112814d6f207edf0d
smit <smitkotkar42@gmail.com>
2023-07-07 20:30:15 +0530

    srg

--- 1_instance.tf
+++ 1_instance.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "my-instance" {
     ami = "ami-0d1c47ab964ae2b87"
-    key_name = "ohio.pem"
+    key_name = "ohio"
     instance_type = "t2.micro"
     vpc_security_group_ids = ["sg-00c1edbd5605281ea"]
 }
